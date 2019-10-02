cc42f51ff539d4eed0958837455044dfc1d4ff98
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-10-02 18:56:51 +0600

    create promo block and added to main page

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import './Promo.css';
+
+const Promo = () => {
+  return (
+    <div className="Promo">
+      <div className="Container"/>
+      <p className="PromoText">Best ever camp for your child</p>
+    </div>
+  )
+};
+
+export default Promo;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Header from "../../components/Header/Header";
+import Promo from "../../components/Promo/Promo";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Promo/>
       </div>
     );
   }
