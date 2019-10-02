ba452caf1e8e206c25bc3e0904b3288bd67c0248
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-10-02 23:00:26 +0600

    added card component to main page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 import Header from "../../components/Header/Header";
 import Promo from "../../components/Promo/Promo";
+import CardOfCamp from "../../components/CardOfCamp/CardOfCamp";
+
+import './Home.css'
+import ListingsForm from "../../components/ListingsForm/ListingsForm";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header/>
-        <Promo/>
+        <Promo>
+          <ListingsForm/>
+        </Promo>
+        <div className="Container">
+          <div className="CardOfCamp">
+            {this.props.listings.map((camp) => (
+              <CardOfCamp
+                key={camp.id}
+                image={camp.objects[4].data.small.jpeg}
+                title={camp.slug}
+                price={camp.price}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  listings: state.listings
+});
+
+export default connect(mapStateToProps, null)(Home);
