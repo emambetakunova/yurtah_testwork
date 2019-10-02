71a1b6eaff1c3d01a2f912d0f8c377f6ac9fe0d7
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-10-02 23:01:34 +0600

    change styles and add props

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-
 import './Promo.css';
 
-const Promo = () => {
+const Promo = ({children}) => {
   return (
     <div className="Promo">
       <div className="Container"/>
       <p className="PromoText">Best ever camp for your child</p>
+      {children}
     </div>
   )
 };
